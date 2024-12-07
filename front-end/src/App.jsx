e183033416e1c7baf0f56ec332c91c2cca8363a7
bravovo <bogdan.nyschyi@gmail.com>
2024-12-07 19:12:36 +0200

    Added node.js express server and added eslint fix for front-end folder

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 function App() {
+    const [data, setData] = useState('');
+
+    const handleClick = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/');
+
+            setData(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
-            <h1>Hello app</h1>
+            <h1>{data}</h1>
+            <button onClick={handleClick}>Request</button>
         </>
     );
 }
